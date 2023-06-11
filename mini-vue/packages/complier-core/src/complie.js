6107309440a5bdef7a186368107bb8c56b2c93e5
codergb <betweent@126.com>
2023-06-11 16:19:39 +0800

    了解transform

--- mini-vue/packages/complier-core/src/complie.js
+++ mini-vue/packages/complier-core/src/complie.js
@@ -2,8 +2,23 @@ import {
   isString
 } from "../../shared/src/general.js";
 import { baseParser } from "./parser.js";
+import { transform } from "./transform.js";
 export function baseComplie(template, options) {
   const isModuleMode = options.mode === "module";
   const ast = isString(template) ? baseParser(template, options) : template;
   console.log(ast);
+  transform(
+    ast,
+    Object.assign(
+      {},
+      options,
+      {
+        nodeTransforms: [],
+        directiveTransform: Object.assign(
+          {},
+          
+        )
+      }
+    )
+  );
 }
---
+++ mini-vue/packages/complier-core/src/transform.js
@@ -0,0 +1,3 @@
+export function transform(root,options) {
+  
+}
